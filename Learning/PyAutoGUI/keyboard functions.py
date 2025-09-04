81f2ba1ee7117dec78291d6edc682e64feeb86a0
Pogbe Emmanuel <mauyonpogbe@gmail.com>
2025-09-04 17:02:48 +0100

    Understood keyboard functions with pyautogui

--- Learning/PyAutoGUI/keyboard functions.py
+++ Learning/PyAutoGUI/keyboard functions.py
@@ -1,1 +1,8 @@
 import pyautogui
+import time
+
+#Keyboard shortcut
+time.sleep(.5)
+# pyautogui.write("Hello World",interval=0.2)
+print(pyautogui.KEYBOARD_KEYS) #Displaying valid keyboard keys that can be used for press() function
+#pyautogui.press("space")
