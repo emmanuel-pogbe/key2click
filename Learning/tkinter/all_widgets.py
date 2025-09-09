9cbe652c98188565a3a752d1338bfaaf7d99e31a
Pogbe Emmanuel <mauyonpogbe@gmail.com>
2025-09-09 15:36:31 +0100

    Created all_widgets.py containing all the widgets in tkinter, I believe

---
+++ Learning/tkinter/all_widgets.py
@@ -0,0 +1,16 @@
+# Widget        Description
+# Button        A button that can cause an action to occur when it is clicked.
+# Canvas        A rectangular area that can be used to display graphics.
+# Checkbutton   A button that may be in either the “on” or “off” position.
+# Entry         An area in which the user may type a single line of input from the keyboard.
+# Frame         A container that can hold other widgets.
+# Label         An area that displays one line of text or an image.
+# Listbox       A list from which the user may select an item
+# Menu          A list of menu choices that are displayed when the user clicks a Menubutton widget.
+# Menubutton    A menu that is displayed on the screen and may be clicked by the user
+# Message       Displays multiple lines of text.
+# Radiobutton   A widget that can be either selected or deselected. Radiobutton widgets usually appear in groups and allow the user to select one of several options.
+# Scale         A widget that allows the user to select a value by moving a slider along a track.
+# Scrollbar     Can be used with some other types of widgets to provide scrolling ability.
+# Text          A widget that allows the user to enter multiple lines of text input.
+# Toplevel      A container, like a Frame, but displayed in its own window.
