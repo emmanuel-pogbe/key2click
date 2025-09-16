6ca32ec24e6a11d83f61beb8037ff1dc33e1089f
Pogbe Emmanuel <mauyonpogbe@gmail.com>
2025-09-16 13:38:08 +0100

    Updated demo script so that user doesn't have to wait 5 seconds for shortcut to render but can click on the spot that user wants to set

--- Learning/demo_script.py
+++ Learning/demo_script.py
@@ -1,19 +1,47 @@
 from pynput.keyboard import GlobalHotKeys
+from pynput.mouse import Listener
 import time
 import pyautogui
-def search():
+import sys
+maps = [] #Points
+shorts = ["f","d","g","t","e"] #Shortcuts that can be set Ctrl + ...
+def stop_script():
+    print("Stopping script...")
+    hotkeys.stop()
+    sys.exit()
+def click_point(index):
+    coordinate_x,coordinate_y = maps[index] 
     pyautogui.moveTo(coordinate_x,coordinate_y)
     pyautogui.leftClick()
-print("Move your mouse to the point you want to set a shortcut for")
-print("You have 5 seconds")
-time.sleep(5)
-coordinate_x,coordinate_y = pyautogui.position()
-print(f"Coordinates -> {coordinate_x},{coordinate_y}")
-print("Press ctrl + f to activate")
-hotkeys = GlobalHotKeys({
-    "<ctrl>+f":search
-})
+def set_default():
+    return tuple(x//2 for x in pyautogui.size())
+def_position = set_default()
+def on_click(x,y,button,pressed):
+    global def_position
+    def_position = x,y
+    return False
+def add_map_point():
+    print("Click (left or right button) a spot you want to set a shortcut for")
+    with Listener(on_click=on_click) as l:
+        l.join()
+    # print(f"Position selected: {def_position}")
+    coordinate_x,coordinate_y = def_position
+    maps.append((coordinate_x,coordinate_y))
+    print(f"Coordinates -> {coordinate_x},{coordinate_y}")
+    print(f"Shortcut set to ctrl + {shorts[len(maps)-1]}")
+while len(maps)<6:
+    print(f"You can add up to 5 points for shortcuts, {len(maps)} shortcuts selected")
+    proceed = input("Press any key to continue")
+    add_map_point()
+    proceed = input("Add another point for a shortcut (y/n)").lower()
+    if proceed != 'y':
+        break
+hotkey_map = {
+    f"<ctrl>+{shorts[i]}": (lambda idx=i: click_point(idx)) for i in range(len(maps))
+}
+hotkey_map["<shift>+<esc>"] = stop_script
+print("Press 'shift + esc' to quit")
+hotkeys = GlobalHotKeys(hotkey_map)
 hotkeys.start()
-hotkeys.wait()
-input()
-hotkeys.stop()
+hotkeys.join()
+print("Thanks for being here!")
--- Learning/pynput/click listener.py
+++ Learning/pynput/click listener.py
@@ -1,17 +1,18 @@
-from pynput.mouse import Button,Listener,Controller
+from pynput.mouse import Listener
 import pyautogui
-import time
 
 IS_SELECTED = False
 def_position = tuple(x//2 for x in pyautogui.size())
 def on_click(x,y,button,pressed):
-    global IS_SELECTED
+    # global IS_SELECTED
     global def_position
-    if IS_SELECTED == True:
-        return False
+    # if IS_SELECTED == True:
+    #     return False
     def_position = x,y
-    IS_SELECTED = True
-print("Click a spot you want to set a shortcut for")
-with Listener(on_click=on_click) as listener:
-    listener.join()
-print(f"Position selected: {def_position}")
+    return False
+def get_a_point():
+    print("Click (left or right button) a spot you want to set a shortcut for")
+    with Listener(on_click=on_click) as listener:
+        listener.join()
+    print(f"Position selected: {def_position}")
+get_a_point()
