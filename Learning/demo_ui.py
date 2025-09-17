3f8d2b99bfadcdfbed7b54406248dd04740a3023
Pogbe Emmanuel <mauyonpogbe@gmail.com>
2025-09-17 23:05:23 +0100

    Semi transparency

--- Learning/demo_ui.py
+++ Learning/demo_ui.py
@@ -4,9 +4,13 @@ class myGui:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Key2Click")
+        self.root.attributes("-topmost",True)
+        # self.root.overrideredirect(True)
+        self.root.attributes("-alpha",0.3) #Semi-transparent
         self.root.geometry("500x500")
-        self.root.iconbitmap("./click icon.ico")
+        self.root.iconbitmap("./Learning/click icon.ico")
         self.main_label = tk.Label(master=self.root,text="Key2Click",font="Calibri 20 bold")
         self.main_label.pack()
+        # self.root.config(cursor="crosshair") #Can be useful later
         self.root.mainloop()
 start = myGui()
